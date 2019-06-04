066d23e09ec57ccce0347c6557c983f5b441f29c
wmzr <psz199616@gmail.com>
2019-06-04 19:23:53 +0800

    first commit

---
+++ src/main/java/com/wmzr/wst/WstApplication.java
@@ -0,0 +1,13 @@
+package com.wmzr.wst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WstApplication.class, args);
+    }
+
+}
